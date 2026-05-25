8b9b14826af7f182c27f7212f9871dd815c2d144
chavezramirez142006@gmail.com <alu_torre1@instructores.senati.local>
2026-05-25 15:04:14 -0500

    banner y index actualizados

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 
-import './App.css'
-
+import Navbar from './components/Navbar/Navbar';
+import './App.css';
+import './index.css';
+import Banner from './components/Banner/Banner';
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      
+      <Navbar />
+      <Banner />
+      {/* El resto de secciones irán aquí debajo.
+Ejemplo: <Footer />
+*/}
     </>
   )
 }
-
-export default App
+export default App;
