8834a8f7a479f00731cd96bc4cf76eee45704690
chavezramirez142006@gmail.com <alu_torre1@instructores.senati.local>
2026-05-27 15:34:51 -0500

    vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,25 @@
 import { defineConfig } from 'vite'
+
 import react from '@vitejs/plugin-react'
 
+
+
+const isGithubPages = process.env.GITHUB_PAGES === 'true'
+
+
+
 // https://vite.dev/config/
+
 export default defineConfig({
-  plugins: [react()],
+
+ base: isGithubPages ? '/portafolio-Villavicencio/' : '/',
+
+ build: {
+
+  outDir: isGithubPages ? 'docs' : 'dist',
+
+ },
+
+ plugins: [react()],
+
 })
